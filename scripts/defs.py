9498d524ca85f3e17caca77a99c7532501185a72
libraun <jetbraun@gmail.com>
2024-06-06 15:25:02 -0500

    Added attributes and methods to 'Ghost' class: attr 'elapsed_ticks' represents number of steps taken since path was first drawn; 'reset()', to reinit ghost

--- scripts/defs.py
+++ scripts/defs.py
@@ -15,14 +15,21 @@ MAP_FILENAME = "../assets/map_WithSpawn.png"
 MAX_GHOSTS = 1
 
 INIT_MANPAC_POSITION = (15, 15)
+INIT_MANPAC_DIRECTION = (1, 0)
 
 
 ## ENUMS ##
 class Tiles(Enum):
     WALL = 0
     FREE = 1
-    NOMANPAC = 2
+    SPAWN = 2
     MANPAC = 3
     GHOST = 4
+    COIN = 5
+    POWERUP = 6
+
+class GameStatus(Enum):
+    GAME_OVER = 0
+    GAME_RUNNING = 1
 
 
--- scripts/ghost.py
+++ scripts/ghost.py
@@ -7,25 +7,44 @@ class Ghost:
         self.is_busy: bool = False
         self.is_alive: bool = True
 
+        self.elapsed_ticks = 0
+
         self._current_position: CoordinatePair = coordinates
         self._path: List[ CoordinatePair ] = None
+
+        self._current_position_last_value = Tiles.SPAWN
 
     def __repr__(self) -> str:
         return str(self._current_position)
     
-    def move(self) -> bool:
+    def move(self, position) -> bool:
 
-        if len(self.path) == 0:
+        if len(self._path) == 0:
             self.is_busy = False
+
+            return None
         else:
+            last_position = self._current_position
+
             self._current_position = self._path.pop(0)
-        return self.is_busy
+            self.elapsed_ticks += 1
+        return last_position, self._current_position_last_value
     
     def set_path(self, path: List[ CoordinatePair ]) -> None:
         self._path = path
+        self.elapsed_ticks = 0
 
     def get_position(self) -> CoordinatePair:
         return self._current_position
+    
+    def set_position(self, position: CoordinatePair) -> None:
+        self._current_position = position
+    
+    def reset(self) -> None:
+        self.elapsed_ticks = 0
+        self.is_busy = False
+
+        self._path = None
         
 
 
