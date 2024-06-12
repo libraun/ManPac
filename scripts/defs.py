de515f2bef7ecd68d3b217b8a9cec6b6ffec80a6
libraun <jetbraun@gmail.com>
2024-06-12 16:43:42 -0500

    Added debugging options to main,defs to prepare for QNet

--- scripts/defs.py
+++ scripts/defs.py
@@ -1,31 +1,39 @@
-from typing import Sequence, Tuple, List, Dict, NamedTuple
+from typing import Tuple, NamedTuple, List, Dict, Sequence
 
 from enum import Enum
 
 ## TYPES ##
+class TransitionMemory(NamedTuple):
+    state: Tuple[bool]
+    action: Tuple[bool]
+    next_action: Tuple[bool]
+    reward: int
+    done: bool
+
+class EXIT_CODES:
+    EXIT_FAILURE = -1
+    EXIT_SUCCESS = 0
+
 class Tiles:
     WALL = 0
     FREE = 1
     SPAWN = 2
-    MANPAC = 3
-    POINT = 4
-    POWERUP = 5
+    POINT = 3
+    POWERUP = 4
+    MANPAC = 5
     GHOST = 6
     GHOST_PLUS_POINT = 7
     GHOST_PLUS_POWERUP = 8
 
-class GameStatus(Enum):
-    GAME_OVER = 0
-    GAME_RUNNING = 1
+class GameStatus:
+    GAME_OVER = False
+    GAME_RUNNING = True
 
 class CoordinatePair(NamedTuple):
     x: int
     y: int
 
 ## CONSTANTS ##
-EXIT_FAILURE = -1
-EXIT_SUCCESS = 0
-
 MAP_FILENAME = "../assets/map_WithSpawn.png"
 
 MAX_GHOSTS = 3
@@ -37,3 +45,21 @@ POINT_COVERAGE = 0.75
 INIT_MANPAC_POSITION = CoordinatePair(15, 15)
 INIT_MANPAC_DIRECTION = CoordinatePair(1, 0)
 
+DIRECTIONS = (
+    CoordinatePair(1, 0),
+    CoordinatePair(-1, 0),
+    CoordinatePair(0, 1),
+    CoordinatePair(0, -1),
+)
+
+## TRAINING ##
+
+MAX_MEMORY = 100000
+BATCH_SIZE = 128
+
+## ETC ##
+
+WINDOW_WIDTH = 620  
+WINDOW_HEIGHT = 620
+
+BLOCKSIZE = 20
--- scripts/main.py
+++ scripts/main.py
@@ -1,53 +1,61 @@
-import pygame as pg
-import random
-
 from Game import Game
-from defs import EXIT_FAILURE, EXIT_SUCCESS, CoordinatePair,Tiles
+from defs import EXIT_CODES, CoordinatePair, Tiles
 
-WINDOW_WIDTH = 620  
-WINDOW_HEIGHT = 620
+from DataViewer import *
+from Model import Model
 
-BLOCKSIZE = 20
+DEBUG_ON = True
 
-COLORS = (
-    pg.Color((255,255,255)),
-    pg.Color((200,200,200)),
-    pg.Color((0,0,255)), 
-    pg.Color((0,255,0)),
-    pg.Color((255,0,0)),
-    pg.Color((255,255,0)),
-    pg.Color((0,255,0)),
-    pg.Color((0,255,255)),
-    pg.Color((255,0,0)),
-    pg.Color((255,0,0)),
-)
+if DEBUG_ON:
 
-def draw_map():
-    global SCREEN,ENV
-    SCREEN.fill((0,0,0))
-    for x in range(WINDOW_WIDTH, BLOCKSIZE):
-        for y in range(WINDOW_HEIGHT, BLOCKSIZE):
+    import pygame as pg
 
-            rect = pg.Rect(x, y, BLOCKSIZE, BLOCKSIZE)
-            pg.draw.rect(SCREEN, pg.Color((200,200,200)), rect,1)
-    
-    ## TODO: Tidy this up...
-    for i, row in enumerate(ENV.get_map().T):
-        for j, cell_value in enumerate(row):
-            rect = pg.Rect(i*BLOCKSIZE, j*BLOCKSIZE, \
-                           BLOCKSIZE, BLOCKSIZE)
-            if cell_value == Tiles.POWERUP or cell_value == Tiles.POINT:
-                pg.draw.rect(SCREEN, COLORS[1], rect)
-                if cell_value == Tiles.POWERUP:
-                    pg.draw.ellipse(SCREEN, COLORS[7], rect, 0)
+    WINDOW_WIDTH = 620  
+    WINDOW_HEIGHT = 620
+
+    BLOCKSIZE = 20
+
+    COLORS = (
+        pg.Color((255,255,255)),
+        pg.Color((200,200,200)),
+        pg.Color((0,0,255)), 
+        pg.Color((0,255,0)),
+        pg.Color((255,0,0)),
+        pg.Color((255,255,0)),
+        pg.Color((0,255,0)),
+        pg.Color((0,255,255)),
+        pg.Color((255,0,0)),
+        pg.Color((255,0,0)),
+    )
+
+    def draw_map():
+        global SCREEN,ENV
+        SCREEN.fill((0,0,0))
+        for x in range(WINDOW_WIDTH, BLOCKSIZE):
+            for y in range(WINDOW_HEIGHT, BLOCKSIZE):
+
+
+                rect = pg.Rect(x, y, BLOCKSIZE, BLOCKSIZE)
+                pg.draw.rect(SCREEN, pg.Color((200,200,200)), rect,1)
+        
+        ## TODO: Tidy this up...
+        for i, row in enumerate(ENV.get_map().T):
+            for j, cell_value in enumerate(row):
+                rect = pg.Rect(i*BLOCKSIZE, j*BLOCKSIZE, \
+                            BLOCKSIZE, BLOCKSIZE)
+                if cell_value == Tiles.POWERUP or cell_value == Tiles.POINT:
+                    pg.draw.rect(SCREEN, COLORS[1], rect)
+                    if cell_value == Tiles.POWERUP:
+                        pg.draw.ellipse(SCREEN, COLORS[7], rect, 0)
+                    else:
+                        pg.draw.circle(SCREEN,COLORS[6],((i * 20) + 10,(j * 20) + 10) , 5)
+                elif cell_value == Tiles.MANPAC:
+                    pg.draw.rect(SCREEN, COLORS[1], rect)
+                    pg.draw.ellipse(SCREEN, COLORS[5], rect, 0)
                 else:
-                    pg.draw.circle(SCREEN,COLORS[6],((i * 20) + 10,(j * 20) + 10) , 5)
-            elif cell_value == Tiles.MANPAC:
-                pg.draw.rect(SCREEN, COLORS[1], rect)
-                pg.draw.ellipse(SCREEN, COLORS[5], rect, 0)
-            else:
-                pg.draw.rect(SCREEN, COLORS[cell_value], rect)
-    pg.display.flip()
+                    pg.draw.rect(SCREEN, COLORS[cell_value], rect)
+        pg.display.flip()
+
 
 DIRECTIONS = (
     CoordinatePair(1, 0),
@@ -56,24 +64,48 @@ DIRECTIONS = (
     CoordinatePair(0, -1),
 )
 
+
+DEBUG_ON = True
+
 if __name__ == "__main__":
 
     ENV = Game()
-    running = True
 
-    pg.init()
+    state_viewer = DataViewer(ENV)
 
-    SCREEN = pg.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
-    clock = pg.time.Clock()
+    model = Model(in_features=13, out_features=4, hidden_dim=256)
+    agent = Agent(model=model,state_memory=StateMemory())
 
-    draw_map()
-    while running:
+    if DEBUG_ON:
+        pg.init()
 
-      #  direction = random.choice(DIRECTIONS)
+        SCREEN = pg.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+        clock = pg.time.Clock()
 
-        result = ENV.play_step(CoordinatePair(1, 0),)
         draw_map()
 
-        clock.tick(5)
+    replay = True
+    running = True
+    while running:
 
-    exit(EXIT_SUCCESS)
+        last_state = state_viewer.get_state()
+        action = agent.get_action(last_state)
+
+        target_direction = DIRECTIONS[action.index(1)]
+
+        game_status,reward = ENV.play_step(target_direction)
+        final_state = state_viewer.get_state()
+        
+        agent.train_step(last_state,final_state,action,reward,game_status)
+
+        if game_status == GameStatus.GAME_OVER:
+            agent.train()
+            agent.n_games += 1
+            if replay:
+                ENV.reset()
+
+        if DEBUG_ON:
+            draw_map()
+            clock.tick(5)
+
+    exit(EXIT_CODES.EXIT_SUCCESS)
