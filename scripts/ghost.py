ccf75cb0269f5a1fd8b1f4b5a07cf930376cc9a0
Jet Braun <jetbraun@gmail.com>
2024-12-05 14:19:39 -0600

    Added comments

--- scripts/ghost.py
+++ scripts/ghost.py
@@ -1,50 +1,50 @@
 from defs import *
+from typing import List
 
 class Ghost:
     
-    def __init__(self, coordinates: CoordinatePair):
+    # Coordinates: A tuple of positive integers representing the ghost's initial
+    # location on the grid.
+    def __init__(self, coordinates: Tuple[int]):
         
+        # Records whether or not this ghost is active (it should always be)
         self.is_busy: bool = True
         self.is_alive: bool = True
 
-        self.elapsed_ticks = 0
-
-        self._current_position: CoordinatePair = coordinates
-        self._path: List[ CoordinatePair ] = []
-    
-    def move_along_path(self) -> None | CoordinatePair:
+        # Can be used to reset a ghosts path (or monitor how long it's been on a path)
+        self.elapsed_ticks: int = 0
         
-        if not self._path:
-            return self._current_position
+        self.position: Tuple[int] = coordinates
+        self.path: List[ CoordinatePair ] = []
 
-        self._current_position = self._path.pop(0)
-
-        self.elapsed_ticks += 1
-        return self._current_position
-    
-    def set_path(self, path: List[ CoordinatePair ]) -> None:
+    def peek(self) -> None | Tuple[int]:
         
-        self.is_busy = True
-        self.elapsed_ticks = 0
+        return None if not self.path else self.path[0]
 
-        self._path = path
-
-    def get_position(self) -> CoordinatePair:
-
-        return self._current_position
+    def move_along_path(self) -> Tuple[int]:
+        
+        # If path is empty (or ghost has been following the same path for too long), 
+        # reset this ghost's path.
+        if not self.path or self.elapsed_ticks > 25:
+            self.reset()
+        # Else, set this ghost's new position to the coordinates at the top of their path.
+        else:
+            self.position = self.path.pop(0)
+            self.elapsed_ticks += 1
+        return self.position
     
-    def set_position(self, position: CoordinatePair) -> None:
-        if self._path:
-            self._path = []
-            self.elapsed_ticks = 0
-        self._current_position = position
+    # Can be used to manually change the ghost's position, 
+    # instead of having it follow its current path to the player
+    def set_position(self, position: Tuple[int]) -> None:
+        self.elapsed_ticks = 0
+        self.position = position
     
     def reset(self) -> None:
 
         self.elapsed_ticks = 0
         self.is_busy = False
 
-        self._path = []
+        self.path.clear()
         
 
 
