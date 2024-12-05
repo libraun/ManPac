67e7721c31c598593c0a63d5d4f43f5c629017fa
Jet Braun <jetbraun@gmail.com>
2024-12-05 14:20:11 -0600

    Added comments; cleaned up 'drawMap()' function

--- scripts/main.py
+++ scripts/main.py
@@ -1,112 +1,87 @@
 import pygame as pg
 
-from Game import Game
-from defs import EXIT_CODES, CoordinatePair, Tiles
+from game import Game
+from defs import DIRECTIONS
 
-from StateViewer import *
-from Model import Model
+from state_viewer import *
+from model import Model
 from agent import Agent
 
-DEBUG_ON = True
+def draw_map():
 
-if DEBUG_ON:
+    global screen, game_env
+    # Reset the screen by filling it with black pixels.
+    screen.fill((0,0,0))
+    for i, row in enumerate(game_env.get_map()):
+        for j, cell in enumerate(row):
+            
+            # Create even-length rectangles, coloring them black if they represent a wall or grey if free space.
+            rect = pg.Rect(i*BLOCKSIZE, j*BLOCKSIZE, BLOCKSIZE, BLOCKSIZE)
+            rect_color = pg.Color((255,255,255)) if cell.is_wall else pg.Color((200,200,200))
 
-    ## Urgent TODO: Fix all this
-    COLORS = (
-        pg.Color((255,255,255)),
-        pg.Color((200,200,200)),
-        pg.Color((0,0,255)), 
-        pg.Color((0,255,0)),
-        pg.Color((255,0,0)),
-        pg.Color((255,255,0)),
-        pg.Color((0,255,0)),
-        pg.Color((0,255,255)),
-        pg.Color((255,0,0)),
-        pg.Color((255,0,0)),
-    )
-    def draw_map():
-        global SCREEN,ENV
-        SCREEN.fill((0,0,0))
-        for x in range(WINDOW_WIDTH, BLOCKSIZE):
-            for y in range(WINDOW_HEIGHT, BLOCKSIZE):
+            # Draw the base tile onto the screen first, so that other items can be rendered over it
+            pg.draw.rect(screen, rect_color, rect)
 
+            # Draw any "character sprites" (pacman or ghosts) 
+            if cell.has_ghost or cell.has_manpac:
 
-                rect = pg.Rect(x, y, BLOCKSIZE, BLOCKSIZE)
-                pg.draw.rect(SCREEN, pg.Color((200,200,200)), rect,1)
-        for i, row in enumerate(ENV.get_map().T):
-            for j, cell_value in enumerate(row):
-                rect = pg.Rect(i*BLOCKSIZE, j*BLOCKSIZE, \
-                            BLOCKSIZE, BLOCKSIZE)
-                if cell_value == Tiles.POWERUP or cell_value == Tiles.POINT:
-                    pg.draw.rect(SCREEN, COLORS[1], rect)
-                    if cell_value == Tiles.POWERUP:
-                        pg.draw.ellipse(SCREEN, COLORS[7], rect, 0)
-                    else:
-                        pg.draw.circle(SCREEN,COLORS[6],((i * 20) + 10,(j * 20) + 10) , 5)
-                
-                else:
-                    pg.draw.rect(SCREEN, COLORS[cell_value], rect)
-        manpac_position = ENV.get_manpac_position()
-        manpac_rect = pg.Rect(manpac_position.y * BLOCKSIZE,manpac_position.x * BLOCKSIZE,BLOCKSIZE,BLOCKSIZE)
-        pg.draw.rect(SCREEN, COLORS[1], rect)
-        pg.draw.ellipse(SCREEN, COLORS[5], manpac_rect, 0)
-        pg.display.flip()
+                character_color = pg.Color((255,255,0)) if cell.has_manpac else pg.Color((0,255,255))
+                pg.draw.ellipse(screen, character_color, rect, 0)
+            # At bottom so that we don't render coins over ghosts
+            elif cell.has_powerup or cell.has_coin:
+
+                item_color = pg.Color((0,255,0)) if cell.has_coin else pg.Color((0,255,255))
+                pg.draw.circle(screen, item_color, ((i*20)+10,(j*20)+10), 5)
+    # Finally, update the display to render all changes.
+    pg.display.flip()
 
 
-DIRECTIONS = (
-    CoordinatePair(1, 0),
-    CoordinatePair(-1, 0),
-    CoordinatePair(0, 1),
-    CoordinatePair(0, -1),
-)
-
-
+# Enables graphics (using Pygame) if true
 DEBUG_ON = True
 
 if __name__ == "__main__":
 
-    ENV = Game()
-    state_viewer = StateViewer(ENV)
+    game_env = Game()
+    state_viewer = DataViewer(game_env)
 
-    model = Model(in_features=22, out_features=4, hidden_dim=256)
+    model = Model(in_features=5, out_features=4, hidden_dim=64)
     agent = Agent(model=model,state_memory=StateMemory())
 
-    if DEBUG_ON:
-        pg.init()
+    pg.init()
 
-        SCREEN = pg.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+    if DEBUG_ON:
+        screen = pg.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
         clock = pg.time.Clock()
 
-        draw_map()
-
-    replay = True
     running = True
 
     top_score = 0
+    current_score = 0
     while running:
 
         last_state = state_viewer.get_state()
-        action = agent.get_action(last_state)
 
+        action = agent.get_action(last_state)
         target_direction = DIRECTIONS[action.index(1)]
 
-        game_status,reward = ENV.play_step(target_direction)
+        game_complete, reward = game_env.play_step(target_direction)
+        current_score += reward
+
         final_state = state_viewer.get_state()
+            
+        agent.train_step(last_state,final_state,action,reward,game_complete)
         
-        agent.train_step(last_state,final_state,action,reward,game_status)
-        
-        agent.state_memory.push(last_state,final_state,action,reward,game_status)
-        if game_status == GameStatus.GAME_OVER:
+        agent.state_memory.push(last_state,final_state,action,reward,game_complete)
+        if game_complete:
 
             agent.train()
             agent.n_games += 1
-            if ENV.score > top_score:
-                top_score = ENV.score
+            if current_score > top_score:
+                top_score = current_score
                 print("New High Score:", top_score)
-            if replay:
-                ENV.reset()
+            game_env.reset()
         if DEBUG_ON:
             draw_map()
-            clock.tick(TICKS_PER_SECOND)
+            clock.tick(50)
 
-    exit(EXIT_CODES.EXIT_SUCCESS)
+    exit(0)
