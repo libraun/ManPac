300738c28e718acae2f3cd737de86d7a7ef028ee
Jet Braun <jetbraun@gmail.com>
2024-12-05 14:20:57 -0600

    Updated tensor shapes to work with CNN output

--- scripts/agent.py
+++ scripts/agent.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from StateViewer import StateMemory
+from state_viewer import StateMemory
 from defs import BATCH_SIZE
 
 class Agent:
@@ -19,46 +19,54 @@ class Agent:
 
 
     def train(self):
-        sample_memory = self.state_memory.sample(batch_size=BATCH_SIZE)
-        state,next_state,action,reward,done = zip(*sample_memory)
-        self.train_step(state,next_state,action,reward,done)
+        if len(self.state_memory.memory) > 5000:
+            mini_sample = random.sample(self.state_memory.memory, 500)
+        else:
+            mini_sample = self.state_memory.memory
+        state,actions,rewards,next_states,done = zip(*mini_sample)
+        self.train_step(state,actions,rewards,next_states,done)
 
     def train_step(self,state,next_state,action,reward,done):
+        state = torch.tensor(state, dtype=torch.float32)
+        next_state = torch.tensor(next_state, dtype=torch.float32)
+                
+        action = torch.tensor(action,dtype=torch.float32)
+        reward = torch.tensor(reward,dtype=torch.float32)
+        if len(action.shape) == 1:
+            state = torch.unsqueeze(state, 0)   
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
 
-        state = torch.tensor(state,dtype=torch.float)
-        next_state = torch.tensor(next_state,dtype=torch.float)
-        action = torch.tensor(action,dtype=torch.float)
-        reward = torch.tensor(reward,dtype=torch.float)
-
-        if len(state.shape) == 1:
-            state = torch.unsqueeze(state, 0)
-            next_state = torch.unsqueeze(next_state,0)
-            action = torch.unsqueeze(action,0)
-            reward = torch.unsqueeze(reward,0)
             done = (done,)
-
+        self.optimizer.zero_grad()
         pred = self.model(state)
-        target = pred.clone()
+        target = pred.clone().detach()
+     #   print(target.shape, done)
         for idx in range(len(done)):
             Q_new = reward[idx]
             if not done[idx]:
-                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+                model_result = self.model(next_state[idx].unsqueeze(0))
+                Q_new = reward[idx] + self.gamma * torch.max(model_result)
             target[idx][torch.argmax(action).item()] = Q_new
-        self.optimizer.zero_grad()
-
-        loss = self.criterion(target,pred)
+        loss = self.criterion(target, pred)
+        
         loss.backward()
 
+        nn.utils.clip_grad_norm_(self.model.parameters(),1)
+        
         self.optimizer.step()
+    
 
     def get_action(self, state):
         final_move = [0,0,0,0]
-        if random.randint(0,200) < (80 - self.n_games):
+        if random.randint(0,200) < (80 - self.n_games): 
             move = random.randint(0,3)
         else:
-            state_vec = torch.tensor(state,dtype=torch.float)
+            state_vec = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
             prediction = self.model(state_vec)
+            move = prediction.argmax().item()
 
-            move = torch.argmax(prediction).item()
+            
         final_move[move] = 1
         return final_move
